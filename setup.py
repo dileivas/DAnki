df3170898ab15b36278d096704d418f3b5249502
Diego Leivas <dileivas@hotmail.com>
2020-04-29 05:07:28 -0300

    altered version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="DAnki",
-    version="1.0.0",
+    version="1.0.1",
     description="Automate deck creation for Anki to learn german",
     long_description=README,
     long_description_content_type="text/markdown",
